5020686b986ff4c2b45625b2dc7daf5f88d83764
Francois Touchard <francois.touchard.wos@gmail.com>
2024-03-19 16:13:19 +0100

    refactor(Player): extracted style from component declaration and removed unnecessary type on image source

--- src/screens/OpeningScreen/components/GameDisplay/GameScreen/Player/index.tsx
+++ src/screens/OpeningScreen/components/GameDisplay/GameScreen/Player/index.tsx
@@ -1,30 +1,31 @@
 import React, { memo } from 'react';
 import { StyleSheet } from 'react-native';
-import FastImage, { Source } from 'react-native-fast-image';
+import FastImage from 'react-native-fast-image';
 
 import { gameDisplayHeight, tileSize, windowWidth } from '../..';
 import { GameScreenTypes } from '../';
 import { usePlayerOrientation } from './usePlayerOrientation';
 
 const PlayerComponent = ({ isPressed }: GameScreenTypes) => {
-  const styles = StyleSheet.create({
-    image: {
-      width: tileSize,
-      height: tileSize,
-      position: 'absolute',
-      top: gameDisplayHeight / 2 - tileSize / 2,
-      left: windowWidth / 2 - tileSize / 2,
-    },
-  });
   const { currentFramePath } = usePlayerOrientation(isPressed);
 
   return (
     <FastImage
       style={styles.image}
-      source={currentFramePath as Source}
+      source={currentFramePath}
       resizeMode={FastImage.resizeMode.contain}
     />
   );
 };
 
 export const Player = memo(PlayerComponent);
+
+const styles = StyleSheet.create({
+  image: {
+    width: tileSize,
+    height: tileSize,
+    position: 'absolute',
+    top: gameDisplayHeight / 2 - tileSize / 2,
+    left: windowWidth / 2 - tileSize / 2,
+  },
+});
