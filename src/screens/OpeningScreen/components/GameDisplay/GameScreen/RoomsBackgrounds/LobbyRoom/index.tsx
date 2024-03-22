49e1aff8b95ec2550ed4757996325e5c9a49100d
Francois Touchard <francois.touchard.wos@gmail.com>
2024-03-22 15:52:50 +0100

    feat(Image-offsets): setting offsets for LobbyRoom

--- src/screens/OpeningScreen/components/GameDisplay/GameScreen/RoomsBackgrounds/LobbyRoom/index.tsx
+++ src/screens/OpeningScreen/components/GameDisplay/GameScreen/RoomsBackgrounds/LobbyRoom/index.tsx
@@ -1,8 +1,42 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import FastImage from 'react-native-fast-image';
 
+import { gameDisplayHeight, tileSize } from '../../..';
+
+export const playerDefaultPositionX = 8;
+export const entrancePositionX = 7;
+export const initialOffsetY =
+  -1 * (gameDisplayHeight - tileSize * 2 - tileSize / 2);
+
 const LobbyRoomComponent = () => {
+  const [initialOffsetX, setInitialOffsetX] = useState(0);
+
+  const goToInitialXOffset = (
+    playerPositionX: number,
+    startingTilePositionX: number,
+  ) => {
+    const offsetToApply = playerPositionX - startingTilePositionX;
+    return -1 * (tileSize * offsetToApply) - tileSize / 2;
+  };
+
+  const styles = StyleSheet.create({
+    image: {
+      flex: 1,
+      position: 'absolute',
+      top: initialOffsetY,
+      right: initialOffsetX,
+      bottom: 0,
+      left: 0,
+    },
+  });
+
+  useEffect(() => {
+    setInitialOffsetX(
+      goToInitialXOffset(playerDefaultPositionX, entrancePositionX),
+    );
+  }, []);
+
   return (
     <FastImage
       style={styles.image}
@@ -11,11 +45,5 @@ const LobbyRoomComponent = () => {
     />
   );
 };
-
-const styles = StyleSheet.create({
-  image: {
-    flex: 1,
-  },
-});
 
 export const LobbyRoom = memo(LobbyRoomComponent);
