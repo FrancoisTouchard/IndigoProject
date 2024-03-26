16d447793ba6685f1bc52ec786eac326513fac90
Francois Touchard <francois.touchard.wos@gmail.com>
2024-03-26 11:16:01 +0100

    style(cleaning): Player cleaning
    renamed and imported config variables

--- src/screens/OpeningScreen/components/GameDisplay/GameScreen/Player/index.tsx
+++ src/screens/OpeningScreen/components/GameDisplay/GameScreen/Player/index.tsx
@@ -2,8 +2,9 @@ import React, { memo } from 'react';
 import { StyleSheet } from 'react-native';
 import FastImage from 'react-native-fast-image';
 
-import { gameDisplayHeight, tileSize, windowWidth } from '../..';
+import { GAME_DISPLAY_HEIGHT, TILE_SIZE, WINDOW_WIDTH } from '../..';
 import { GameScreenTypes } from '../';
+import { GAP_FIXER_TILE } from '../RoomsBackgrounds/tileMapHelpers';
 import { usePlayerOrientation } from './usePlayerOrientation';
 
 const PlayerComponent = ({ isPressed }: GameScreenTypes) => {
@@ -22,10 +23,10 @@ export const Player = memo(PlayerComponent);
 
 const styles = StyleSheet.create({
   image: {
-    width: tileSize,
-    height: tileSize,
+    width: TILE_SIZE,
+    height: TILE_SIZE,
     position: 'absolute',
-    top: gameDisplayHeight / 2 - tileSize / 2,
-    left: windowWidth / 2 - tileSize / 2,
+    top: GAME_DISPLAY_HEIGHT / 2 - GAP_FIXER_TILE,
+    left: WINDOW_WIDTH / 2 - GAP_FIXER_TILE,
   },
 });
