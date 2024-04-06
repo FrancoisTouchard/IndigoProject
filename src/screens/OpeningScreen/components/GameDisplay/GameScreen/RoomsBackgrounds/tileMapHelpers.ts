f38f903501cbc33dfd2d84bb541e8529699a1419
FrancoisTouchard <114145869+FrancoisTouchard@users.noreply.github.com>
2024-04-06 17:29:25 +0200

    fix(tileMapHelpers): fixing findPlayerPositionWithOffset (#16)
    adding TILE_SIZE_HALVED to XY coordinate calculations

--- src/screens/OpeningScreen/components/GameDisplay/GameScreen/RoomsBackgrounds/tileMapHelpers.ts
+++ src/screens/OpeningScreen/components/GameDisplay/GameScreen/RoomsBackgrounds/tileMapHelpers.ts
@@ -3,6 +3,7 @@ import { LOBBYROOM_MAP_Y_COUNT } from './LobbyRoom/LobbyRoomMap';
 
 export const TILE_SIZE_DOUBLED = TILE_SIZE * 2;
 export const TILE_SIZE_HALVED = TILE_SIZE * 0.5;
+export const DIRECTION = -1;
 
 /**
  * @const DEFAULT_OFFSET_X
@@ -16,17 +17,19 @@ export const findPlayerPositionWithOffset = (
 ) => {
   const DEFAULT_OFFSET_Y = goToInitialOffsetY();
 
-  /**
-   * @const YOffsetsSubstracted
-   * Gère le cas où Ycoordinate = 0 pour retourner 0 au lieu de -1
-   */
-  const YOffsetsSubstracted = Math.floor(DEFAULT_OFFSET_Y - offsetY);
-  const Ycoordinate =
-    YOffsetsSubstracted === 0
-      ? Math.floor((DEFAULT_OFFSET_Y - offsetY) / TILE_SIZE)
-      : Math.floor((DEFAULT_OFFSET_Y - offsetY) / TILE_SIZE) * -1 - 1;
+  const YcoordinateOffsetInTiles = Math.floor(
+    (DEFAULT_OFFSET_Y - offsetY - TILE_SIZE_HALVED) / TILE_SIZE,
+  );
 
-  const Xcoordinate = Math.floor((DEFAULT_OFFSET_X - offsetX) / TILE_SIZE);
+  /**
+   * @const Ycoordinate
+   * Convertit offsetY en positif et lui attribue l'emplacement correspondant dans l'array LobbyRoomMap
+   */
+  const Ycoordinate = YcoordinateOffsetInTiles * DIRECTION - 1;
+
+  const Xcoordinate = Math.floor(
+    (DEFAULT_OFFSET_X - offsetX + TILE_SIZE_HALVED) / TILE_SIZE,
+  );
 
   return { Ycoordinate, Xcoordinate };
 };
@@ -42,9 +45,8 @@ export const goToInitialOffsetY = () => {
    * Hauteur de la map en pixels = nombre de tiles * valeur en pixels d'une tile
    */
   const MapHeightInPixels = LOBBYROOM_MAP_Y_COUNT * TILE_SIZE;
-  const direction = -1;
 
-  return direction * (MapHeightInPixels / 2 - TILE_SIZE_HALVED);
+  return DIRECTION * (MapHeightInPixels / 2 - TILE_SIZE_HALVED);
 };
 
 /**
