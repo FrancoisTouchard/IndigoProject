1a337cc21b27c251025a775675a08d59172c02b0
Francois Touchard <francois.touchard.wos@gmail.com>
2024-04-11 10:03:16 +0200

    build(adding-image): asset and preload

--- src/screens/OpeningScreen/components/GameDisplay/AnimationOpening/usePreloadImages.tsx
+++ src/screens/OpeningScreen/components/GameDisplay/AnimationOpening/usePreloadImages.tsx
@@ -23,6 +23,8 @@ export const usePreloadImages = (redirectToNextScreen: () => void) => {
       require('../../../../../../assets/Player/RedGoingDown2.png'),
       require('../../../../../../assets/Player/RedGoingUp1.png'),
       require('../../../../../../assets/Player/RedGoingUp2.png'),
+      // MenuScreens
+      require('../../../../../../assets/MenusScreens/menuTemplateLarge.png'),
     ];
 
     const uris = images.map(image => {
