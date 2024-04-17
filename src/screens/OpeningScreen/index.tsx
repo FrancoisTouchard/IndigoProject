2b7ebf50b9f60d4b242dfa0cb7dd2331139be427
Francois Touchard <francois.touchard.wos@gmail.com>
2024-04-17 19:19:11 +0200

    refactor(player-position): updating hooks
    updating useDirectionalCross and creating position helpers

--- src/screens/OpeningScreen/index.tsx
+++ src/screens/OpeningScreen/index.tsx
@@ -1,37 +1,24 @@
-import React, { memo, useEffect, useRef } from 'react';
+import React, { memo } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { GameControls } from './components/GameControls';
-import { ArrowType } from './components/GameControls/DirectionalCross/DirectionalArrow/types';
 import { useDirectionalCross } from './components/GameControls/DirectionalCross/useDirectionalCross';
 import { GameDisplay } from './components/GameDisplay';
-import { COLLISION_OVERLAP_IN_PIXELS } from './components/GameDisplay/GameScreen/RoomsBackgrounds/LobbyRoom/LobbyRoomMap';
+import { getPlayerPosition } from './components/GameDisplay/GameScreen/PlayerPositionIndicator/playerPositionHelpers';
 import { useLobbyRoom } from './components/GameDisplay/GameScreen/RoomsBackgrounds/LobbyRoom/useLobbyRoom';
-import { findTileAllocation } from './components/GameDisplay/GameScreen/RoomsBackgrounds/tileAllocationHelpers';
-import { findPlayerPositionWithOffset } from './components/GameDisplay/GameScreen/RoomsBackgrounds/tileMapHelpers';
 import { usePlayerCurrentInteraction } from './components/GameDisplay/GameScreen/RoomsBackgrounds/usePlayerCurrentInteraction';
 import { useGameStateManager } from './components/GameDisplay/useGameStateManager';
 
 const OpeningScreenComponent = () => {
   const { gameState, redirectToGameScreen } = useGameStateManager();
-  const { isPressed, panResponder } = useDirectionalCross();
+  const { isPressed, panResponder, isPressedPreviousValue } =
+    useDirectionalCross();
   const {
     isPressed: isPressedInteraction,
     panResponder: panResponderInteraction,
   } = useDirectionalCross();
   const { offsetY, offsetX } = useLobbyRoom(isPressed);
-
-  const { currentTileYArrayCoordinate, currentTileXArrayCoordinate } =
-    findPlayerPositionWithOffset(
-      offsetY + COLLISION_OVERLAP_IN_PIXELS,
-      offsetX - COLLISION_OVERLAP_IN_PIXELS,
-    );
-  const isPressedPreviousValue = useRef<ArrowType | false>(false);
-
-  const currentTileAllocation = findTileAllocation(
-    currentTileYArrayCoordinate,
-    currentTileXArrayCoordinate,
-  );
+  const { currentTileAllocation } = getPlayerPosition(offsetY, offsetX);
   const {
     playerCurrentInteraction,
     handlePlayerCurrentInteraction,
@@ -41,12 +28,6 @@ const OpeningScreenComponent = () => {
     isPressed,
     isPressedPreviousValue,
   );
-
-  useEffect(() => {
-    if (isPressed) {
-      isPressedPreviousValue.current = isPressed;
-    }
-  }, [isPressed]);
 
   return (
     <View style={styles.gameAndControlsContainer}>
