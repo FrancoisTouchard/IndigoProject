d0e5ca11729d8750d42d93d5e9425416be264251
Francois Touchard <francois.touchard.wos@gmail.com>
2024-06-26 20:23:25 +0200

    refactor(PcMenu): Modyfing ListSelector implementation

---
+++ src/screens/OpeningScreen/components/GameDisplay/GameScreen/PCMenu/ListSelector/index.tsx
@@ -0,0 +1,32 @@
+import React, { memo } from 'react';
+import { StyleSheet } from 'react-native';
+import FastImage from 'react-native-fast-image';
+
+interface ListSelectorProps {
+  leftOffset: number;
+}
+
+const LIST_SELECTOR_DIMENSIONS = 10;
+
+const ListSelectorComponent = ({ leftOffset }: ListSelectorProps) => {
+  return (
+    <FastImage
+      style={[
+        styles.listSelector,
+        { left: -leftOffset + LIST_SELECTOR_DIMENSIONS / 2 },
+      ]}
+      source={require('../../../../../../../../assets/ResizableMenu/ListSelector.png')}
+      resizeMode={FastImage.resizeMode.contain}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  listSelector: {
+    height: LIST_SELECTOR_DIMENSIONS,
+    width: LIST_SELECTOR_DIMENSIONS,
+    position: 'absolute',
+  },
+});
+
+export const ListSelector = memo(ListSelectorComponent);
