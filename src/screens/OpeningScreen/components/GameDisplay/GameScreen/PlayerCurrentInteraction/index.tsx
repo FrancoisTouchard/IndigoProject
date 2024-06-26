0288e6d5ab3145dc8e89864af81afb422d3440c3
Francois Touchard <francois.touchard.wos@gmail.com>
2024-06-26 20:26:59 +0200

    refactor(PcMenu): modifying props implementation

--- src/screens/OpeningScreen/components/GameDisplay/GameScreen/PlayerCurrentInteraction/index.tsx
+++ src/screens/OpeningScreen/components/GameDisplay/GameScreen/PlayerCurrentInteraction/index.tsx
@@ -1,30 +1,20 @@
 import React, { memo } from 'react';
 
 import { ArrowType } from '../../../GameControls/DirectionalCross/DirectionalArrow/types';
-import { PcInteraction } from './PcInteraction';
+import { PCMenu } from '../PCMenu';
 
 interface PlayerCurrentInteractionProps {
   playerCurrentInteraction: string | null;
   isPressed: ArrowType | false;
-  focusedMenuItem: number;
-  clickedMenuItem: string | null;
 }
 
 const PlayerCurrentInteractionComponent = ({
   playerCurrentInteraction,
   isPressed,
-  focusedMenuItem,
-  clickedMenuItem,
 }: PlayerCurrentInteractionProps) => {
   if (playerCurrentInteraction !== 'APC') return null;
 
-  return (
-    <PcInteraction
-      isPressed={isPressed}
-      focusedMenuItem={focusedMenuItem}
-      clickedMenuItem={clickedMenuItem}
-    />
-  );
+  return <PCMenu isPressed={isPressed} />;
 };
 
 export const PlayerCurrentInteraction = memo(PlayerCurrentInteractionComponent);
