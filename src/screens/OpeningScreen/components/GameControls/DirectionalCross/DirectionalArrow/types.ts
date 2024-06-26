584db81b8e35262ea0368df86a7f3be6dee6149b
Francois Touchard <francois.touchard.wos@gmail.com>
2024-06-26 20:10:21 +0200

    refactor(PcMenu): modifying types

--- src/screens/OpeningScreen/components/GameControls/DirectionalCross/DirectionalArrow/types.ts
+++ src/screens/OpeningScreen/components/GameControls/DirectionalCross/DirectionalArrow/types.ts
@@ -1,6 +1,13 @@
+import { IsPressedType } from '../../../GameDisplay/types';
+
 export type ArrowType = 'up' | 'down' | 'left' | 'right';
+export type ABButtonType = 'A' | 'B';
+/**
+ * ajout du type StartSel pour plus tard
+ */
+// export type StartSelButton = 'Start' | 'Select';
 
 export interface DirectionalArrowProps {
   arrowType: ArrowType;
-  isPressed: false | ArrowType;
+  isPressed: IsPressedType;
 }
