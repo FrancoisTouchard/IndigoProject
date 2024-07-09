468b82b335803889ca52ef68603651848a031271
Francois Touchard <francois.touchard.wos@gmail.com>
2024-07-09 18:47:12 +0200

    style(style-config-file): creating assets/appStyle.js to store style config variables

---
+++ assets/appStyle.js
@@ -0,0 +1,23 @@
+const white = '#FAF9F6';
+const black = '#000000';
+const turquoise = '#1d7a82';
+const pokédexBorderColor = '#1e81b0';
+const pokédexBackgroundColor = '#F9F6EE';
+
+const pokémonFont = 'PokemonClassic';
+
+const menuFontSize = 10;
+const pokédexBorderFontSize = 10;
+const pokédexFontSize = 10;
+
+export {
+  white,
+  black,
+  turquoise,
+  pokédexBorderColor,
+  pokédexBackgroundColor,
+  pokémonFont,
+  menuFontSize,
+  pokédexBorderFontSize,
+  pokédexFontSize,
+};
